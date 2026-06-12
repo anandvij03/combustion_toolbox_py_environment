7ba6c2d39d65e2cde178a6465d5249c1f61954da
ts85901 <250784390+anandvij03@users.noreply.github.com>
2026-06-12 13:52:13 +0530

    .

---
+++ python_port/combustiontoolbox/core/__init__.py
@@ -0,0 +1,9 @@
+
+from .CaloricGasModel.CaloricGasModel import CaloricGasModel
+from .ChemicalSystem.ChemicalSystem import ChemicalSystem, findProducts, setListSpecies
+from .Elements.Elements import Elements
+from .EquationState.EquationState import EquationState
+from .EquationStateIdealGas.EquationStateIdealGas import EquationStateIdealGas
+from .EquationStatePengRobinson.EquationStatePengRobinson import EquationStatePengRobinson
+from .Mixture.Mixture import Mixture, print, setStagnation
+from .Species.Species import getAdiabaticIndex, getEnthalpy, getEntropy, getGibbsEnergy, getHeatCapacityPressure, getHeatCapacityVolume, getInternalEnergy, getThermalEnthalpy, getThermalInternalEnergy, Species
