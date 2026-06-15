ad99733b7f3469a9d50c8ce0326aa7b3ac27cc1b
Sacheth135 <24110308@iitgn.ac.in>
2026-06-15 15:25:00 +0530

    change

---
+++ python_port/combustiontoolbox/examples/Example_TV.py
@@ -0,0 +1,29 @@
+import numpy as np
+from combustiontoolbox.databases.NasaDatabase.NasaDatabase import NasaDatabase
+from combustiontoolbox.core import *
+from combustiontoolbox.equilibrium import *
+
+# Get Nasa database
+DB = NasaDatabase()
+
+# Define chemical system
+system = ChemicalSystem(DB)
+
+# Initialize mixture
+mix = Mixture(system)
+
+# Define chemical state
+mix.set(['CH4'], 'fuel', 1)
+mix.set(['N2', 'O2', 'Ar', 'CO2'], 'oxidizer', np.array([78.084, 20.9476, 0.9365, 0.0319]) / 20.9476)
+
+# Define properties
+mixArray = mix.setProperties('temperature', 3000, 'volume', 1, 'equivalenceRatio', np.arange(0.5, 5.01, 0.01))
+
+# Initialize solver
+solver = EquilibriumSolver(problemType='TV')
+
+# Solve the problem
+solver.solveArray(mixArray)
+
+# Generate report
+solver.report(mixArray)
