1481ee2b043ea569e8687597c2e8b8d9453118c7
ts85901 <250784390+anandvij03@users.noreply.github.com>
2026-06-15 16:37:37 +0530

    Example_TP Functional
    The Example_TP functionality is given here

---
+++ python_port/combustiontoolbox/examples/Example_TP.py
@@ -0,0 +1,46 @@
+# -------------------------------------------------------------------------
+# EXAMPLE: TP
+#
+# Compute equilibrium composition at defined temperature (T = 3000 K) and
+# pressure (p = 1.01325 bar) for lean to rich CH4-air mixtures, and a set
+# of equivalence ratios phi contained in (0.5, 5) [-]
+#   
+# See wiki or setListspecies method from ChemicalSystem class for predefined
+# sets of species
+#
+# @author: Anand V
+# @adapted-from: Alberto Cuadra Lara
+#                 
+# Last update June 2026
+# -------------------------------------------------------------------------
+
+# Import packages
+import numpy as np
+from combustiontoolbox.databases.NasaDatabase.NasaDatabase import NasaDatabase
+from combustiontoolbox.core import *
+from combustiontoolbox.equilibrium import *
+
+# Get Nasa database
+DB = NasaDatabase()
+
+# Define chemical system
+system = ChemicalSystem(DB)
+
+# Initialize mixture
+mix = Mixture(system)
+
+# Define chemical state
+mix.set(['CH4'], 'fuel', 1)
+mix.set(['N2', 'O2', 'Ar', 'CO2'], 'oxidizer', np.array([78.084, 20.9476, 0.9365, 0.0319]) / 20.9476)
+
+# Define properties
+mixArray = mix.setProperties('temperature', 3000, 'pressure', 1 * 1.01325, 'equivalenceRatio', np.arange(0.5, 5.01, 0.01))
+
+# Initialize solver
+solver = EquilibriumSolver(problemType='TP')
+
+# Solve problem
+solver.solveArray(mixArray)
+
+# Generate report
+solver.report(mixArray)
